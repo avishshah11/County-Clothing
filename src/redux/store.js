325f473563b2bf8a65b72cb4d543643ab8fee9a9
Avish Shah <avishshah1108@gmail.com>
2020-11-12 02:25:12 +0530

    redux added

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const Middleware = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...Middleware));
+
+export default store;
