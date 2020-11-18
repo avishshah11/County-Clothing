63bcf1f359f8354f2147bb2d98bb1e84bcb3a166
Avish Shah <avishshah1108@gmail.com>
2020-11-19 03:25:44 +0530

    adding checkout page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import './App.css';
 
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignup from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import HomePage from './pages/homepage/homepage.component';
+
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -46,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route excat path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />
           ) : (
               <SignInAndSignup />
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,7 +2,7 @@ import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
-export const slectCartItems = createSelector(
+export const selectCartItems = createSelector(
     [selectCart],
     cart => cart.cartItems
 );
@@ -13,9 +13,17 @@ export const selectCartHidden = createSelector(
 );
 
 export const selectCartItemsCount = createSelector(
-    [slectCartItems],
+    [selectCartItems],
     cartItems => cartItems.reduce(
         (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+        0
+    )
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity * cartItem.price,
         0
     )
 );
