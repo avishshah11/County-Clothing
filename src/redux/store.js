2282e5e64c16492dd223a2928186b875ae84d5cd
Avish Shah <avishshah1108@gmail.com>
2020-11-19 20:10:26 +0530

    persist storage added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 const Middleware = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...Middleware));
+export const store = createStore(rootReducer, applyMiddleware(...Middleware));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
