50c9936d797c90d9a3a1128c5fbd71cb50695aa0
Avish Shah <avishshah1108@gmail.com>
2020-11-24 00:03:02 +0530

    deploying shopdata from server to app

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Route } from 'react-router-dom'; 
+import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collection-overview/collections-overview.component';
 import CollectionPage  from '../collection/collection.component';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
-const ShopPage  = ({ match }) => (
-        <div className = 'shop-page'>
-           <Route exact path = {`${match.path}`} component ={CollectionsOverview}/>
-           <Route path = {`${match.path}/:collectionId`} component = {CollectionPage}/>
-        </div>
-);
+class ShopPage  extends React.Component {
+        unsubscribeFromSnapshot = null;
 
-export default ShopPage;
+        componentDidMount(){
+                const { updateCollections } = this.props;
+                const collectionRef = firestore.collection('collections');
+
+                collectionRef.onSnapshot( async snapshot => {
+                       const collectionMap =  convertCollectionsSnapshotToMap(snapshot);
+                       updateCollections(collectionMap);
+                });
+        }
+        render(){
+                const { match } = this.props;
+                return(
+                         <div className = 'shop-page'>
+                        <Route exact path = {`${match.path}`} component ={CollectionsOverview}/>
+                        <Route path = {`${match.path}/:collectionId`} component = {CollectionPage}/>
+                        </div>
+                        );
+                
+        }
+} 
+
+const mapDispatchToProps = dispatch => ({
+        updateCollections: collectionMap => dispatch(updateCollections(collectionMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
