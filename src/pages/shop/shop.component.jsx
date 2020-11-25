d921d925dbb05931fea10116e23e40553c402e15
Avish Shah <avishshah1108@gmail.com>
2020-11-26 01:54:34 +0530

    selectors added

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,7 +6,7 @@ import { createStructuredSelector } from 'reselect';
 import CollectionsOverview from '../../components/collection-overview/collections-overview.component';
 import CollectionPage  from '../collection/collection.component';
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
@@ -19,14 +19,14 @@ class ShopPage  extends React.Component {
                 fetchCollectionsStartAsync();  
         }
         render(){
-                const { match, isCollectionFetching } = this.props;
+                const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
                 return(
                          <div className = 'shop-page'>
                         <Route exact path = {`${match.path}`} render = {(props) => ( <CollectionsOverviewWithSpinner isLoading = {isCollectionFetching}{...props}/>
                         )}
                         />
                         <Route path = {`${match.path}/:collectionId`} render = {(props) => ( 
-                        <CollectionPageWithSpinner isLoading = {isCollectionFetching}{...props}/>
+                        <CollectionPageWithSpinner isLoading = {!isCollectionsLoaded}{...props}/>
                         )}
                         />
                         </div>
@@ -36,7 +36,8 @@ class ShopPage  extends React.Component {
 } 
 
 const mapStateToProps = createStructuredSelector({
-        isCollectionFetching: selectIsCollectionFetching 
+        isCollectionFetching: selectIsCollectionFetching,
+        isCollectionsLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
