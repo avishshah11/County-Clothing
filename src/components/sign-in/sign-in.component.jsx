3913e093df6c48ad58539beb8477592f64960a4e
Avish Shah <avishshah1108@gmail.com>
2020-12-01 22:37:58 +0530

    google sign in component re-structured using redux-sagas

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 import FromInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { googleSignInStart } from '../../redux/user/user.actions';
+
 import './sign-in.styles.scss';
+import react from 'react';
 
 class SignIn extends React.Component{
     constructor(props){
@@ -37,6 +41,7 @@ class SignIn extends React.Component{
     };
 
     render(){
+        const { googleSignInStart } = this.props;
         return (
             <div className = 'sign-in'>
                 <h2> Already have an account!</h2>
@@ -62,7 +67,8 @@ class SignIn extends React.Component{
                     />
                     <div className = 'buttons'>
                         <CustomButton type='submit'> Sign In </CustomButton>
-                        <CustomButton onClick = {signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type = 'button'
+                        onClick = {googleSignInStart} isGoogleSignIn>
                             SIGN IN WITH GOOGLE
                         </CustomButton>
                     </div>    
@@ -72,4 +78,8 @@ class SignIn extends React.Component{
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart())
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
